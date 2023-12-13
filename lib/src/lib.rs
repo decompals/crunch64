e01778da3c54a4f4288a0d2b6133f590d94ee0b3
Angie <angheloalf95@gmail.com>
2023-12-13 16:00:46 -0300

    Start setting up pyo3 stuff

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -56,3 +56,23 @@ impl CompressionType {
         }
     }
 }
+
+#[cfg(feature = "python_bindings")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python_bindings")]
+#[pyfunction]
+/// Formats the sum of two numbers as string.
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[cfg(feature = "python_bindings")]
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn crunch64(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
