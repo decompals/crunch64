b32f09c43065f32ea57265e8e0d0465a39c50098
angie <angheloalf95@gmail.com>
2023-12-15 09:55:15 -0300

    decompals as the author

--- lib/crunch64/__init__.py
+++ lib/crunch64/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 # Version should be synced with lib/Cargo.toml and lib/pyproject.toml
 __version_info__ = (0, 1, 0)
 __version__ = ".".join(map(str, __version_info__))
-__author__ = "Capybara man" # TODO: Ethan update this before release plz
+__author__ = "decompals"
 
 from . import yay0 as yay0
 from . import yaz0 as yaz0
