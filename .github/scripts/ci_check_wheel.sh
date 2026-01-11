b83624a7d016f0fef65d64bb72be9d550503ddea
Anghelo Carvajal <angheloalf95@gmail.com>
2026-01-10 22:02:38 -0300

    fix

--- .github/scripts/ci_check_wheel.sh
+++ .github/scripts/ci_check_wheel.sh
@@ -5,8 +5,8 @@ KEY=$2
 set -e
 
 rm -rf .venv
-uv venv -p $PYTHON_VERSION
+uv venv -p $PYTHON_VERSION --managed-python
 source .venv/bin/activate
 uv run python --version
-uv pip install $(find ./dist/ -name "crunch64-*-$KEY-*") --managed-python
+uv pip install $(find ./dist/ -name "crunch64-*-$KEY*")
 uv run python -c "import crunch64; print(crunch64.__version__)"
